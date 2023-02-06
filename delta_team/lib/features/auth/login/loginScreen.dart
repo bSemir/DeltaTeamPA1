3483e3c524bb0c314f46c7db9d38b5687d5531a5
Amar <amar@Amars-Air.localdomain>
2023-02-06 16:56:36 +0100

    removed folder

--- delta_team/lib/features/auth/login/loginScreen.dart
+++ delta_team/lib/features/auth/login/loginScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -6,6 +7,9 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Container(
+      child: Image.asset('assets/images/vector.png'),
+    ));
   }
 }
