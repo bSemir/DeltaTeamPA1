3bfc30ebf8e024e68c06a2ff6a2e9bc5bb9e8dc9
Amar <amar@Amars-Air.localdomain>
2023-02-06 16:44:48 +0100

    first commit

---
+++ delta_team/lib/features/auth/signup/signupWidget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SignupWidget extends StatelessWidget {
+  const SignupWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ delta_team/lib/models/model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+//ovo cemo dodavati zavisno od modela koji nam bude trebao
+class Model extends StatelessWidget {
+  const Model({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
